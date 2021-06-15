f30d1fc47d952ccad3757e2e2f769bfeb00f81ca
Felipe-M-dev <39914824+Felipe-M-dev@users.noreply.github.com>
2021-06-14 21:34:44 -0400

    Update escape.rb
    - add diagram

--- escape.rb
+++ escape.rb
@@ -1,3 +1,12 @@
+# INICIO
+# leer parámetro g = gravedad
+# leer parámetro r = radio * 1000
+# mostrar operación matecática raíz sobre 2 * g * r
+# FIN
+
 g = ARGV[0].to_f
-r = ARGV[1].to_f * 1000
-puts Math.sqrt(2 * g * r)
+r = ARGV[1].to_i * 1000
+puts "Gravedad: #{g} KM"
+puts "Radio: #{r} M"
+escape = Math.sqrt(2 * g * r)
+puts "El resultado es: #{escape.round(2)} Mts/s"
